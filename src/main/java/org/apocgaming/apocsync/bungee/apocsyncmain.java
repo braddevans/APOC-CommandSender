186627ae83bd6b5d00dea36292c8a04b2ef6abea
braddevans <braddevans123@gmail.com>
2017-12-17 16:45:59 +0000

    updated code

---
+++ src/main/java/org/apocgaming/apocsync/bungee/apocsyncmain.java
@@ -0,0 +1,19 @@
+package org.apocgaming.apocsync.bungee;
+
+import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class apocsyncmain extends Plugin {
+
+    @Override
+    public void onEnable() {
+        BungeeCord.getInstance().getPluginManager().registerCommand(this, new CommandClass());
+        BungeeCord.getInstance().registerChannel("Return");
+    }
+
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
