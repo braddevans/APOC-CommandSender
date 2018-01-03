808c1a18a1adb9549d58dbbd139ab5f70f6d2c0a
braddevans123@gmail.com <bc123456789>
2018-01-03 23:50:51 +0000

    commit

--- src/main/java/org/apocgaming/apocsync/bukkit/Main.java
+++ src/main/java/org/apocgaming/apocsync/bukkit/Main.java
@@ -2,7 +2,9 @@ package org.apocgaming.apocsync.bukkit;
 
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
-
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,22 +13,37 @@ import org.bukkit.plugin.messaging.PluginMessageListener;
 public class Main extends JavaPlugin implements PluginMessageListener {
 	
 	public void onEnable() {
-		 Bukkit.getMessenger().registerIncomingPluginChannel(this, "Return", this);
+		Bukkit.getMessenger().registerIncomingPluginChannel(this, "Return", this);
 	}
 
 	@Override
 	public void onPluginMessageReceived(String channel, Player p, byte[] message) {
-		
+
 		DataInputStream in = new DataInputStream(new ByteArrayInputStream(message));
-		
-		try {
+
+		 try{
+			//strings
 			String sub = in.readUTF();
-			if (sub.equals("command")) {
-				String cmd = in.readUTF();
+			String cmd = in.readUTF();
+			//end of strings
+
+			if (cmd.toLowerCase().equals("op")) {
+				System.out.println("[Apoc Sync] Received a command message from BungeeCord, Command: /op, not executing it.");
+			}
+
+			if (cmd.toLowerCase().equals("stop")) {
+				System.out.println("[Apoc Sync] Received a command message from BungeeCord, Command: /stop , not executing it.");
+			}
+
+			if (cmd.toLowerCase().equals("restart")) {
+				System.out.println("[Apoc Sync] Received a command message from BungeeCord, Command: /restart, not executing it.");
+			}
+
+			else if (sub.equals("command")) {
 				System.out.println("[Apoc Sync] Received a command message from BungeeCord, executing it.");
 				getServer().dispatchCommand(getServer().getConsoleSender(), cmd);
-				
 			}
+
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
