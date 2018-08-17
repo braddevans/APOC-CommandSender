b82f3992013b4fe0e053af84a0903b767a9e84a4
bread <braddevans123@gmail.com>
2018-08-17 21:33:07 +0200

    murr update

---
+++ Bukkit/src/main/java/org/apocgaming/apocsync/bukkit/Main.java
@@ -0,0 +1,45 @@
+package org.apocgaming.apocsync.bukkit;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+
+public class Main extends JavaPlugin implements PluginMessageListener {
+
+    public void onEnable() {
+        Bukkit.getMessenger().registerIncomingPluginChannel(this, "Return", this);
+    }
+
+    @Override
+    public void onPluginMessageReceived(String channel, Player p, byte[] message) {
+
+        DataInputStream in = new DataInputStream(new ByteArrayInputStream(message));
+
+        try {
+            //strings
+            String sub = in.readUTF();
+            //end of strings
+
+            if (sub.equals("command")) {
+                if (sub.toString().toLowerCase().equals("op")) {
+                    p.sendMessage("[ApocCommandSync]: Received a command, Command: /op, not executing it.");
+                } else if (sub.toString().toLowerCase().equals("stop")) {
+                    p.sendMessage("[ApocCommandSync]: Received a command, Command: /stop, not executing it.");
+                } else if (sub.toString().toLowerCase().equals("restart")) {
+                    p.sendMessage("[ApocCommandSync]: Received a command, Command: /restart, not executing it.");
+                } else {
+                    p.sendMessage("[Apoc Sync] Received a command message from BungeeCord, executing it.");
+                    getServer().dispatchCommand(getServer().getConsoleSender(), sub);
+                    p.sendMessage("cmd: " + sub + "   |in lowercase and to a string|   " + sub.toLowerCase().toString());
+                }
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
